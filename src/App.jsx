cfa01aea58361ab4c776762b44f1575661282df4
Louise Wong <lw17122@bristol.ac.uk>
2022-02-16 11:33:51 +0000

    trial with mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import WeatherBox from "./components/WeatherBox/WeatherBox";
+import "./assets/data/data"
+import weather from "./assets/data/data";
 
 const App = () => {
   return (
     <div className="App">
-      <WeatherBox />
+      <WeatherBox location={weather.location.name} temperature={weather.current.temp_c} icon={weather.current.condition.icon} description={weather.current.condition.text}/>
     </div>
   );
 };
--- src/components/WeatherBox/WeatherBox.jsx
+++ src/components/WeatherBox/WeatherBox.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./WeatherBox.scss";
 
-const WeatherBox = () => {
+const WeatherBox = (props) => {
+  const {location, temperature, icon, description} = props
   return (
-    <div>
+    <div className="weather-box">
       <h1>Enter your locations</h1>
       <form>
         <input type="text" id="location" name="location" />
         <input type="submit" value="Submit" />
       </form>
+      <div className="weather-box__current">
+        <h2>{location}</h2>
+        <img src={icon}/>
+        <p>Current temperature is {temperature}°C and {description}</p>
+      </div>
+
     </div>
   );
 };
