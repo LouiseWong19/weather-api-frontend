f4dbe5d03cba578b2681add0d6988a313d8a42b2
Louise Wong <lw17122@bristol.ac.uk>
2022-02-16 11:49:46 +0000

    added styling for history and weather boxes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.scss";
 import WeatherBox from "./components/WeatherBox/WeatherBox";
 import "./assets/data/data"
 import weather from "./assets/data/data";
+import HistoryBox from "./components/HistoryBox/HistoryBox";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <WeatherBox location={weather.location.name} temperature={weather.current.temp_c} icon={weather.current.condition.icon} description={weather.current.condition.text}/>
+      <HistoryBox />
     </div>
   );
 };
--- src/components/HistoryBox/HistoryBox.jsx
+++ src/components/HistoryBox/HistoryBox.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import "./HistoryBox.scss";
 
 const HistoryBox = () => {
-  return <div>HistoryBox</div>;
+  return <div className="history-box">
+    <h2>Search History</h2>
+    </div>;
 };
 
 export default HistoryBox;
